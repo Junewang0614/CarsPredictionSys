cb93f1b2a72ec2f8c9626c4a0094ad38105c952e
Junewang0614 <1554717543@qq.com>
2022-01-03 13:43:21 +0800

    Initial commit

---
+++ CarsPredictionSys/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for CarsPredictionSys project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'CarsPredictionSys.settings')
+
+application = get_asgi_application()
